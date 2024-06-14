0fb56fa5414e156492ffb06922a0cbc108498216
Dildora <48212@edu.raseko.fi>
2024-06-14 14:40:32 +0300

    lisätty app goods ja templates

--- main/views.py
+++ main/views.py
@@ -2,11 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-   context: {
+   context: dict[str, str] ={
        'title': 'Savesta -Etusivu',
-       'content': 'SAVESTASSA TEHTYJÄ TUOTTEITA'
+       'content': 'SAVESTASSA TEHTYJÄ TUOTTEITA',
    }
    return render(request, 'main/index.html', context) 
 
 def about(request):
-   return HttpResponse('About page') 
+   context: dict[str, str] ={
+       'title': 'Meidän historia',
+       'content': 'Meidän historia',
+       'content_text': 'Meidän historia on pitkä ja monivaiheinen. Olemme tehneet tuotteita jo vuodesta 1999.',
+   }
+   return render(request, 'main/about.html', context)  
