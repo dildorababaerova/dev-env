a8f3f811a7f8653ae7e674893eb2568b1a44b087
Dildora <48212@edu.raseko.fi>
2024-06-17 14:46:11 +0300

    muutos

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, Product
 
 # Register your models here.
+# admin.site.register(Category)
+# admin.site.register(Product)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    search_fields = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'discount_price', 'quantity', 'category')
+    search_fields = ('name', 'category')
+    list_filter = ('category', 'name', 'price', 'discount_price', 'quantity')
+    prepopulated_fields = {'slug': ('name',)}
--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Kategoria')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+
+    class Meta:
+        verbose_name = 'Kategoria'
+        verbose_name_plural = 'Kategoriat'
+
+    def __str__(self):
+        return self.name 
+
+
+class Product(models.Model):
+    name=models.CharField(max_length=150, verbose_name='Nimi')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description=models.TextField(blank=True, null=True, verbose_name='Kuvaus')
+    price=models.DecimalField(default=0.00, max_digits=9, decimal_places=2, verbose_name='Hinta')
+    discount_price=models.DecimalField(default=0.00, max_digits=9, decimal_places=2, verbose_name='Alennettu hinta')    
+    image=models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Kuva')
+    quantity=models.IntegerField(default=0, verbose_name='Määrä')
+    category=models.ForeignKey(to=Category, on_delete=models.CASCADE, verbose_name='Kategoria')
+
+
+    class Meta:
+        verbose_name = 'Tuote'
+        verbose_name_plural = 'Tuotteet' 
+
+    def __str__(self):
+        return self.name
