96a27b229613418e0dcfa5e43b2259621e1f6b72
Dildora <48212@edu.raseko.fi>
2024-06-20 11:25:46 +0300

    Slug converter > product

--- goods/views.py
+++ goods/views.py
@@ -4,15 +4,23 @@ from .models import Product
 def catalog(request):
 
     goods = Product.objects.all()
-    context:dict[str, str] = {
+    context = {
         'title': 'Etusivu-Catalog',
         'goods': goods,
     }
     return render(request, 'goods/catalog.html', context)
 
 
-def product(request):
-   
+def product(request, product_slug=False, product_id=False):
+
+    if product_id:
+        product = Product.objects.get(id=product_id)
+    else:
+        product = Product.objects.get(slug=product_slug)
+
+    context = {
+        'product': product,
+    }
     
-    return render(request, 'goods/product.html')
+    return render(request, 'goods/product.html', context=context)
 
