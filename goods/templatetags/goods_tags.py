056fe93f4f46878b1edbca0ccaf6687c6b7ca062
Dildora <48212@edu.raseko.fi>
2024-06-25 19:18:15 +0300

    Tuotteet filteroitu ja urlencode

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,10 +1,19 @@
 from django import template
+from django.utils.http import urlencode
+
 from goods.models import Category
+
 
 
 register = template.Library()
 
 @register.simple_tag
-
 def tag_category():
     return Category.objects.all()
+
+
+@register.simple_tag(takes_context=True)
+def change_param(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
--- goods/views.py
+++ goods/views.py
@@ -5,13 +5,21 @@ from .models import Product
 def catalog(request, category_slug):
 
     page = request.GET.get('page', 1)
-    
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
+
     if category_slug == 'kaikki-tuotteet':
         goods = Product.objects.all()
     else:
         goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
+
+    if on_sale:
+        goods = goods.filter(discount_price__gt=0)
+
+    if order_by and order_by != 'default':
+        goods = goods.order_by(order_by)
     
-    paginator = Paginator(goods, 3)
+    paginator = Paginator(goods, 12)
     current_page = paginator.page(page)
 
     context = {
