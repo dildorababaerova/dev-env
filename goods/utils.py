c5ba27dff59270517c262c02f85438cd0feb5361
Dildora <48212@edu.raseko.fi>
2024-06-27 11:59:28 +0300

    Update utils.py

--- goods/utils.py
+++ goods/utils.py
@@ -6,7 +6,7 @@ def q_search(query):
     if query.isdigit() and len(query) <=5:
         return Product.objects.filter(id=int(query))
     
-    keywords= [word for word in query.split() if len(word)>=1] 
+    keywords= [word for word in query.split() if len(word)>=2] 
     q_objects=Q()
 
     for token in keywords:
