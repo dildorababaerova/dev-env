0b43fe756736730e6b639e24d966252831aaef62
Dildora <48212@edu.raseko.fi>
2024-07-05 15:52:56 +0300

    cart_add muokattu

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,34 @@
+from goods.models import Product
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class CartQueryset(models.QuerySet):
+    def total_price(self):
+        return sum(cart.products_price() for cart in self)
+    
+    def total_quantity(self):
+        if self:
+            return sum(cart.quantity for cart in self)
+        return 0
+
+class Cart(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True, verbose_name='Käyttäjä')
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE, verbose_name='Tuote')
+    quantity = models.PositiveSmallIntegerField(default=0, verbose_name='Määrä')
+    session_key= models.CharField(max_length=32, null=True, blank=True)
+    created_timestamp = models.DateTimeField(auto_now_add=True, verbose_name='Lisätty aika')
+    
+    class Meta:
+        db_table = 'cart'
+        verbose_name = 'kori'
+        verbose_name_plural = 'korit'
+
+    objects = CartQueryset.as_manager()
+
+    def products_price(self):
+        return round(self.product.sell_price() * self.quantity, 2)
+        
+    def __str__(self):
+        return f'Kori {self.user.username} | Tuote {self.product.name} | Määrä {self.quantity} | Lisätty aika {self.created_timestamp}' 
--- carts/views.py
+++ carts/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 
-def cart_add(request, product_id):
+
+from carts.models import Cart
+from goods.models import Product
+
+
+def cart_add(request, product_slug):
+    product= Product.objects.get(slug=product_slug)
+    
+    if request.user.is_authenticated:
+        carts = Cart.objects.filter(product=product, user=request.user)
+
+        if carts.exists():
+            cart = carts.first()
+            cart.quantity += 1
+            cart.save()
+    else:
+        cart = Cart.objects.create(product=product, user=request.user, quantity=1)
+
+        
+
+    return redirect(request.META.get('HTTP_REFERER'))
+
+
+def cart_change(request, product_slug):
     ...
 
-
-def cart_change(request, product_id):
-    ...
-
-def cart_remove(request, product_id):
+def cart_remove(request):
     ...
